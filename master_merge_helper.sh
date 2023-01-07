27afc91c8b3c51bfbde34bd02027f969db648202
Zhijia Cao <caozhijia@126.com>
2023-01-07 16:59:58 +0800

    [TimechoDB][To rel/1.0] add cherry pick script

---
+++ master_merge_helper.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+function setup_env() {
+	HAS_MASTER_IOTDB=$(git branch -vv | grep "cherry_pick_1.0")
+	if [ -z "${HAS_MASTER_IOTDB}" ]
+	then
+		echo "Create branch cherry_pick_1.0"
+		git remote add iotdb https://github.com/apache/iotdb.git
+		git fetch --quiet iotdb rel/1.0
+		git checkout --quiet FETCH_HEAD
+		git checkout -b cherry_pick_1.0
+	else
+		git checkout cherry_pick_1.0
+	fi
+	echo "Pull iotdb rel/1.0"
+	git pull --quiet iotdb rel/1.0
+
+	HAS_MASTER=$(git branch -vv | grep "rel/1.0")
+	if [ -z "${HAS_MASTER}" ]
+	then
+		echo "Create branch rel/1.0"
+		git fetch --quiet origin rel/1.0
+		git checkout --quiet FETCH_HEAD
+		git checkout -b rel/1.0
+	else
+		git checkout rel/1.0
+	fi
+	echo "Pull origin rel/1.0"
+	git pull --quiet origin rel/1.0
+}
+
+function cherry_pick() {
+	git cherry-pick $1
+	if [ $? -ne 0 ]
+	then
+    	echo "Please resolve conflicts manually."
+    	exit 1
+	fi
+}
+
+setup_env
+
+# Checkout to rel/1.0
+git checkout rel/1.0
+
+PRIVATE_COMMIT_PREFIX="TimechoDB"
+
+# Find the last community commit
+LAST_COMMUNITY_COMMIT_MSG=$(git log --pretty=format:"%s" | grep -v "${PRIVATE_COMMIT_PREFIX}" | head -1)
+if [ -z "${LAST_COMMUNITY_COMMIT_MSG}" ]
+then
+	echo "Failed to find the last community commit"
+	exit 1
+fi
+echo "Last community commit: ${LAST_COMMUNITY_COMMIT_MSG}"
+
+LAST_COMMUNITY_COMMIT_HASH=$(git log --pretty=format:"%h %s" cherry_pick_1.0 | grep -F "${LAST_COMMUNITY_COMMIT_MSG}" | awk '{print $1}')
+if [ -z "${LAST_COMMUNITY_COMMIT_HASH}" ]
+then
+	echo "Failed to find the hash of the last community commit"
+	exit 1
+fi
+echo "Last community commit hash: ${LAST_COMMUNITY_COMMIT_HASH}"
+
+# Cherry-pick new commits one by one
+NUM_COMMITS=$(git log --pretty=format:"%h" cherry_pick_1.0 | grep -B 10000 "${LAST_COMMUNITY_COMMIT_HASH}" | grep -v "${LAST_COMMUNITY_COMMIT_HASH}" | wc -l)
+echo "Found ${NUM_COMMITS} commit(s) to cherry-pick"
+echo ""
+
+for COMMIT_HASH in $(git log --pretty=format:"%h" cherry_pick_1.0 | grep -B 10000 "${LAST_COMMUNITY_COMMIT_HASH}" | grep -v "${LAST_COMMUNITY_COMMIT_HASH}" | tac)
+do
+	echo "Working on:"
+	echo $(git show --oneline --quiet ${COMMIT_HASH})
+	while true; do
+		read -p "  Input command (summary/detail/cp/skip): " CMD
+		case ${CMD} in
+			summary ) git show --stat ${COMMIT_HASH};;
+            detail  ) git show ${COMMIT_HASH};;
+			cp      ) cherry_pick ${COMMIT_HASH}; break;;
+            skip    ) break;;
+			*       ) echo "  Invalid command";;
+		esac
+	done
+	echo ""
+done
+
