de0c2e0c2c00fb7c7152a0ec75d0eaf38ac563ca
Liu Xuxin <37140360+THUMarkLau@users.noreply.github.com>
2023-05-31 21:34:47 +0800

    Add metrics for thread cpu consumption (#9833)

--- server/src/main/java/org/apache/iotdb/db/engine/StorageEngine.java
+++ server/src/main/java/org/apache/iotdb/db/engine/StorageEngine.java
@@ -214,8 +214,7 @@ public class StorageEngine implements IService {
   public void recover() {
     setAllSgReady(false);
     cachedThreadPool =
-        IoTDBThreadPoolFactory.newCachedThreadPool(
-            ThreadName.STORAGE_ENGINE_CACHED_SERVICE.getName());
+        IoTDBThreadPoolFactory.newCachedThreadPool(ThreadName.STORAGE_ENGINE_CACHED_POOL.getName());
 
     List<Future<Void>> futures = new LinkedList<>();
     asyncRecover(futures);
@@ -237,7 +236,8 @@ public class StorageEngine implements IService {
               checkResults(futures, "StorageEngine failed to recover.");
               setAllSgReady(true);
               ttlMapForRecover.clear();
-            });
+            },
+            ThreadName.STORAGE_ENGINE_RECOVER_TRIGGER.getName());
     recoverEndTrigger.start();
   }
 
@@ -326,7 +326,8 @@ public class StorageEngine implements IService {
 
     recover();
 
-    ttlCheckThread = IoTDBThreadPoolFactory.newSingleThreadScheduledExecutor("TTL-Check");
+    ttlCheckThread =
+        IoTDBThreadPoolFactory.newSingleThreadScheduledExecutor(ThreadName.TTL_CHECK.getName());
     ScheduledExecutorUtil.safelyScheduleAtFixedRate(
         ttlCheckThread,
         this::checkTTL,
@@ -406,7 +407,7 @@ public class StorageEngine implements IService {
       }
     }
     syncCloseAllProcessor();
-    ThreadUtils.stopThreadPool(ttlCheckThread, ThreadName.TTL_CHECK_SERVICE);
+    ThreadUtils.stopThreadPool(ttlCheckThread, ThreadName.TTL_CHECK);
     ThreadUtils.stopThreadPool(
         seqMemtableTimedFlushCheckThread, ThreadName.TIMED_FLUSH_SEQ_MEMTABLE);
     ThreadUtils.stopThreadPool(
