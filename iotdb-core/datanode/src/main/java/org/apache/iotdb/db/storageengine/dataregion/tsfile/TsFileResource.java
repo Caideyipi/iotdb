ac9ccdbd75de351f56ed909b50e96966ac9ea460
Itami Sho <42286868+MiniSho@users.noreply.github.com>
2023-08-18 17:31:00 +0800

    [IOTDB-6122] Pipe: Progress Index markers an incorrect value when load a tsfile with tsfileResource (#10908)
    When we load a tsfile, if it has a tsFileResource, there are two cases in tsFileResource:
    
    1. it doesn't have a progress index, we just give it one from system.
    2. it has a progress index, the index shouldn't be loaded, it should be recovered instead of updating.

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/TsFileResource.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/dataregion/tsfile/TsFileResource.java
@@ -1226,6 +1226,14 @@ public class TsFileResource {
             : maxProgressIndex.updateToMinimumIsAfterProgressIndex(progressIndex));
   }
 
+  public void recoverProgressIndex(ProgressIndex progressIndex) {
+    if (progressIndex == null) {
+      return;
+    }
+
+    maxProgressIndex = progressIndex;
+  }
+
   public ProgressIndex getMaxProgressIndexAfterClose() throws IllegalStateException {
     if (getStatus().equals(TsFileResourceStatus.UNCLOSED)) {
       throw new IllegalStateException(
