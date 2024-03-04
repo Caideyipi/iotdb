a30e723f8948119858b73fa57d2b91b144b9b95f
Yongzao <yongzao@apache.org>
2024-03-04 12:00:51 +0800

    [IOTDB-6301] Optimize insert first (#12080)
    Co-authored-by: Xiangpeng Hu <65238551+BUAAserein@users.noreply.github.com>

--- iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/load/LoadManager.java
+++ iotdb-core/confignode/src/main/java/org/apache/iotdb/confignode/manager/load/LoadManager.java
@@ -49,6 +49,7 @@ import org.apache.iotdb.confignode.manager.partition.RegionGroupStatus;
 import org.apache.iotdb.confignode.rpc.thrift.TConfigNodeInfo;
 import org.apache.iotdb.confignode.rpc.thrift.TNodeActivateInfo;
 import org.apache.iotdb.confignode.rpc.thrift.TTimeSlotList;
+import org.apache.iotdb.tsfile.utils.Pair;
 
 import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
@@ -471,5 +472,15 @@ public class LoadManager {
    */
   public void removeRegionRouteCache(TConsensusGroupId regionGroupId) {
     loadCache.removeRegionRouteCache(regionGroupId);
+  }
+
+  /** Force balance the region leader and broadcast RouteChangeEvent if necessary. */
+  public void forceBalanceRegionLeader() {
+    Map<TConsensusGroupId, Pair<Integer, Integer>> differentRegionLeaderMap =
+        routeBalancer.balanceRegionLeader();
+    Map<TConsensusGroupId, Pair<TRegionReplicaSet, TRegionReplicaSet>> differentRegionPriorityMap =
+        routeBalancer.balanceRegionPriority();
+    statisticsService.broadcastRouteChangeEventIfNecessary(
+        differentRegionLeaderMap, differentRegionPriorityMap);
   }
 }
