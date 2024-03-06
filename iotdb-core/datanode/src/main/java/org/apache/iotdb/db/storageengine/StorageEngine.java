f4243134d7dc68d2b3d6d86c3969e45217ba0e42
Haonan <hhaonan@outlook.com>
2024-03-06 21:38:02 +0800

    Fix fail to read memTable ids from the wal file (#12132)

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/StorageEngine.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/storageengine/StorageEngine.java
@@ -649,7 +649,7 @@ public class StorageEngine implements IService {
   public void operateFlush(TFlushReq req) {
     if (req.storageGroups == null) {
       StorageEngine.getInstance().syncCloseAllProcessor();
-      WALManager.getInstance().deleteOutdatedFilesInWALNodes();
+      WALManager.getInstance().syncDeleteOutdatedFilesInWALNodes();
     } else {
       for (String storageGroup : req.storageGroups) {
         if (req.isSeq == null) {
