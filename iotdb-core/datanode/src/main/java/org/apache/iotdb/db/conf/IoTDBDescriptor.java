86e2b66c7a95c9cbaed3bbc7f59b37930582e817
YuFengLiu <38746920+l2280212@users.noreply.github.com>
2024-04-14 14:45:57 +0800

    new iot_consensus_throttle_threshold_in_byte (#12333)
    spotless

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBDescriptor.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBDescriptor.java
@@ -52,6 +52,7 @@ import org.apache.iotdb.db.utils.DateTimeUtils;
 import org.apache.iotdb.db.utils.datastructure.TVListSortAlgorithm;
 import org.apache.iotdb.metrics.config.MetricConfigDescriptor;
 import org.apache.iotdb.metrics.config.ReloadLevel;
+import org.apache.iotdb.metrics.metricsets.system.SystemMetrics;
 import org.apache.iotdb.metrics.reporter.iotdb.IoTDBInternalMemoryReporter;
 import org.apache.iotdb.metrics.reporter.iotdb.IoTDBInternalReporter;
 import org.apache.iotdb.metrics.utils.InternalReporterType;
@@ -75,10 +76,12 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileStore;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class IoTDBDescriptor {
@@ -88,6 +91,14 @@ public class IoTDBDescriptor {
   private final CommonDescriptor commonDescriptor = CommonDescriptor.getInstance();
 
   private final IoTDBConfig conf = new IoTDBConfig();
+
+  private static final long MAX_THROTTLE_THRESHOLD = 600 * 1024 * 1024 * 1024L;
+
+  private static final long MIN_THROTTLE_THRESHOLD = 50 * 1024 * 1024 * 1024L;
+
+  private static final double MAX_DIR_USE_PROPORTION = 0.8;
+
+  private static final double MIN_DIR_USE_PROPORTION = 0.5;
 
   protected IoTDBDescriptor() {
     loadProps();
@@ -322,8 +333,6 @@ public class IoTDBDescriptor {
                 .trim()));
 
     initMemoryAllocate(properties);
-
-    loadWALProps(properties);
 
     String systemDir = properties.getProperty("dn_system_dir");
     if (systemDir == null) {
@@ -977,6 +986,8 @@ public class IoTDBDescriptor {
     commonDescriptor.loadCommonProps(properties);
     commonDescriptor.initCommonConfigDir(conf.getSystemDir());
 
+    loadWALProps(properties);
+
     // Timed flush memtable
     loadTimedService(properties);
 
@@ -1415,7 +1426,7 @@ public class IoTDBDescriptor {
         Long.parseLong(
             properties.getProperty(
                 "iot_consensus_throttle_threshold_in_byte",
-                Long.toString(conf.getThrottleThreshold())));
+                Long.toString(getThrottleThresholdWithDirs())));
     if (throttleDownThresholdInByte > 0) {
       conf.setThrottleThreshold(throttleDownThresholdInByte);
     }
@@ -1428,6 +1439,33 @@ public class IoTDBDescriptor {
     if (cacheWindowInMs > 0) {
       conf.setCacheWindowTimeInMs(cacheWindowInMs);
     }
+  }
+
+  public long getThrottleThresholdWithDirs() {
+    ArrayList<String> dataDiskDirs = new ArrayList<>(Arrays.asList(conf.getDataDirs()));
+    ArrayList<String> walDiskDirs =
+        new ArrayList<>(Arrays.asList(commonDescriptor.getConfig().getWalDirs()));
+    Set<FileStore> dataFileStores = SystemMetrics.getFileStores(dataDiskDirs);
+    Set<FileStore> walFileStores = SystemMetrics.getFileStores(walDiskDirs);
+    double dirUseProportion = 0;
+    dataFileStores.retainAll(walFileStores);
+    // if there is no common disk between data and wal, use more usableSpace.
+    if (dataFileStores.isEmpty()) {
+      dirUseProportion = MAX_DIR_USE_PROPORTION;
+    } else {
+      dirUseProportion = MIN_DIR_USE_PROPORTION;
+    }
+    long newThrottleThreshold = Long.MAX_VALUE;
+    for (FileStore fileStore : walFileStores) {
+      try {
+        newThrottleThreshold = Math.min(newThrottleThreshold, fileStore.getUsableSpace());
+      } catch (IOException e) {
+        LOGGER.error("Failed to get file size of {}, because", fileStore, e);
+      }
+    }
+    newThrottleThreshold = (long) (newThrottleThreshold * dirUseProportion * walFileStores.size());
+    // the new throttle threshold should between MIN_THROTTLE_THRESHOLD and MAX_THROTTLE_THRESHOLD
+    return Math.max(Math.min(newThrottleThreshold, MAX_THROTTLE_THRESHOLD), MIN_THROTTLE_THRESHOLD);
   }
 
   private void loadAutoCreateSchemaProps(Properties properties) {
