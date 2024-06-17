2007e5ee99e888dea06dccb88ca4385e17fb1ecc
Haiming Zhu <heimingch@gmail.com>
2024-06-17 01:06:04 +0000

    [TIMECHODB][TIMECHODB] Get tsfile remote path via local cache

--- object-storage/src/main/java/com/timecho/iotdb/os/HybridFileInputFactoryDecorator.java
+++ object-storage/src/main/java/com/timecho/iotdb/os/HybridFileInputFactoryDecorator.java
@@ -53,8 +53,7 @@ public class HybridFileInputFactoryDecorator implements FileInputFactory {
   public TsFileInput getTsFileInput(String filePath) throws IOException {
     File file = new File(filePath);
     if (!file.exists() && ObjectStorageConnector.getConnector().isConnectorEnabled()) {
-      return fileInputFactory.getTsFileInput(
-          FSUtils.parseLocalTsFile2OSFile(file, config.getBucketName(), dataNodeId).getPath());
+      return fileInputFactory.getTsFileInput(getTsFileRemotePath(file, dataNodeId).getPath());
     }
     return fileInputFactory.getTsFileInput(filePath);
   }
