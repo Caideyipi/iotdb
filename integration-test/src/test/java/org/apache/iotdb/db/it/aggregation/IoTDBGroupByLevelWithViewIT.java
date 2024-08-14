b33ab8302d13acb5e3f3fc0f8eac67a59fbdce00
Gaofei Cao <gaofei.cao@timecho.com>
2024-08-14 12:30:00 +0000

    [TIMECHODB][TIMECHODB] Add annotation for view it

--- integration-test/src/test/java/org/apache/iotdb/db/it/aggregation/IoTDBGroupByLevelWithViewIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/aggregation/IoTDBGroupByLevelWithViewIT.java
@@ -20,14 +20,21 @@
 package org.apache.iotdb.db.it.aggregation;
 
 import org.apache.iotdb.it.env.EnvFactory;
+import org.apache.iotdb.it.framework.IoTDBTestRunner;
+import org.apache.iotdb.itbase.category.ClusterIT;
+import org.apache.iotdb.itbase.category.LocalStandaloneIT;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
 
 import static org.apache.iotdb.db.it.utils.TestUtils.prepareData;
 import static org.apache.iotdb.db.it.utils.TestUtils.resultSetEqualTest;
 
+@RunWith(IoTDBTestRunner.class)
+@Category({LocalStandaloneIT.class, ClusterIT.class})
 public class IoTDBGroupByLevelWithViewIT {
 
   private static final String[] SQLs =
