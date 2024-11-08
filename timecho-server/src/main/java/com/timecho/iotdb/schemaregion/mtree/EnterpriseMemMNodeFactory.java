b99073bd856de17c17e9a89346082d9eceb6f1b1
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-11-08 09:56:03 +0800

    [TIMECHODB] Fixed deviceInfo compilation

--- timecho-server/src/main/java/com/timecho/iotdb/schemaregion/mtree/EnterpriseMemMNodeFactory.java
+++ timecho-server/src/main/java/com/timecho/iotdb/schemaregion/mtree/EnterpriseMemMNodeFactory.java
@@ -30,7 +30,7 @@ import org.apache.iotdb.db.schemaengine.schemaregion.mtree.impl.mem.mnode.impl.A
 import org.apache.iotdb.db.schemaengine.schemaregion.mtree.impl.mem.mnode.impl.BasicInternalMNode;
 import org.apache.iotdb.db.schemaengine.schemaregion.mtree.impl.mem.mnode.impl.DatabaseMNode;
 import org.apache.iotdb.db.schemaengine.schemaregion.mtree.impl.mem.mnode.impl.MeasurementMNode;
-import org.apache.iotdb.db.schemaengine.schemaregion.mtree.impl.mem.mnode.info.DeviceInfo;
+import org.apache.iotdb.db.schemaengine.schemaregion.mtree.impl.mem.mnode.info.TreeDeviceInfo;
 
 import com.timecho.iotdb.schemaregion.EnterpriseSchemaConstant;
 import com.timecho.iotdb.schemaregion.mtree.mem.LogicalViewMNode;
@@ -48,7 +48,7 @@ public class EnterpriseMemMNodeFactory implements IMNodeFactory<IMemMNode> {
   @Override
   public IDeviceMNode<IMemMNode> createDeviceMNode(IMemMNode parent, String name) {
     BasicInternalMNode internalMNode = new BasicInternalMNode(parent, name);
-    internalMNode.setDeviceInfo(new DeviceInfo<>());
+    internalMNode.setDeviceInfo(new TreeDeviceInfo<>());
     return internalMNode.getAsDeviceMNode();
   }
 
@@ -60,7 +60,7 @@ public class EnterpriseMemMNodeFactory implements IMNodeFactory<IMemMNode> {
   @Override
   public IMemMNode createDatabaseDeviceMNode(IMemMNode parent, String name) {
     DatabaseMNode databaseMNode = new DatabaseMNode(parent, name);
-    databaseMNode.setDeviceInfo(new DeviceInfo<>());
+    databaseMNode.setDeviceInfo(new TreeDeviceInfo<>());
     return databaseMNode;
   }
 
