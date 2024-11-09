6cedebc56d57061614ed7c8c90479dd70a8d0a11
Caideyipi <87789683+Caideyipi@users.noreply.github.com>
2024-11-09 16:06:16 +0800

    Implement drop column for table model

--- iotdb-protocol/thrift-confignode/src/main/thrift/confignode.thrift
+++ iotdb-protocol/thrift-confignode/src/main/thrift/confignode.thrift
@@ -1081,6 +1081,12 @@ struct TShowTableResp {
    2: optional list<TTableInfo> tableInfoList
 }
 
+struct TDescTableResp {
+   1: required common.TSStatus status
+   2: optional binary tableInfo
+   3: optional set<string> preDeletedColumns
+}
+
 struct TFetchTableResp {
    1: required common.TSStatus status
    2: optional binary tableInfoMap
@@ -1851,6 +1857,8 @@ service IConfigNodeRPCService {
   common.TSStatus alterOrDropTable(TAlterOrDropTableReq req)
 
   TShowTableResp showTables(string database, bool isDetails)
+
+  TDescTableResp describeTable(string database, string tableName, bool isDetails)
 
   TFetchTableResp fetchTables(map<string, set<string>> fetchTableMap)
 
