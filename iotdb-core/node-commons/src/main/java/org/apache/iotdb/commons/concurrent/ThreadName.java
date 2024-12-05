49021064c46270080d2255c5bdc9c05d6084d66b
Potato <tanxinyu@apache.org>
2024-12-05 13:21:48 +0800

    Add Binary Allocator (#14321)
    * add binary allocator
    
    * fix
    
    * add gc evictor and hot load configuration
    
    * fix
    
    * fix
    
    * add license
    
    * resolve comments
    
    * add comments
    
    * modified adaptive weighted average
    
    * fix unit test
    
    * add necessary comments
    
    * restruct package
    
    Signed-off-by: OneSizeFitQuorum <tanxinyu@apache.org>
    
    * restruct SizeClasses
    
    Signed-off-by: OneSizeFitQuorum <tanxinyu@apache.org>
    
    * enhance
    
    Signed-off-by: OneSizeFitQuorum <tanxinyu@apache.org>
    
    * refactor eviction thread and remove evict timer
    
    * almost finish
    
    Signed-off-by: OneSizeFitQuorum <tanxinyu@apache.org>
    
    * start binary allocator with hot loading
    
    * add config check
    
    * fix compile error
    
    * enhance
    
    Signed-off-by: OneSizeFitQuorum <tanxinyu@apache.org>
    
    * Remove duplicate test
    
    Signed-off-by: OneSizeFitQuorum <tanxinyu@apache.org>
    
    ---------
    
    Signed-off-by: OneSizeFitQuorum <tanxinyu@apache.org>
    Co-authored-by: MrQuansy <siyiquan2021@gmail.com>

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/concurrent/ThreadName.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/concurrent/ThreadName.java
@@ -197,6 +197,7 @@ public enum ThreadName {
 
   REPAIR_DATA("RepairData"),
   FILE_TIME_INDEX_RECORD("FileTimeIndexRecord"),
+  BINARY_ALLOCATOR_SAMPLE_EVICTOR("BinaryAllocator-SampleEvictor"),
 
   // the unknown thread name is used for metrics
   UNKOWN("UNKNOWN");
