999e9d4d2d9e72f8bdf6d14f3ecbfd785403ff17
Haonan <hhaonan@outlook.com>
2025-01-10 19:10:24 +0800

    Clone value array when insert non aligned tablet with null (#14643)
    * Clone value array when insert non aligned tablet with null
    
    * Fix UT
    
    * Add recover wal IT

--- integration-test/src/main/java/org/apache/iotdb/it/env/cluster/env/AbstractEnv.java
+++ integration-test/src/main/java/org/apache/iotdb/it/env/cluster/env/AbstractEnv.java
@@ -1126,6 +1126,11 @@ public abstract class AbstractEnv implements BaseEnv {
   }
 
   @Override
+  public void shutdownForciblyAllDataNodes() {
+    dataNodeWrapperList.forEach(AbstractNodeWrapper::stopForcibly);
+  }
+
+  @Override
   public void ensureNodeStatus(
       final List<BaseNodeWrapper> nodes, final List<NodeStatus> targetStatus)
       throws IllegalStateException {
