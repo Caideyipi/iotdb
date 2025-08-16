161dd64210cfc1670779d4692e3f258ab2d7ce7f
Yongzao <yongzao@apache.org>
2025-08-16 11:16:59 +0800

    [AINode] Update test env and dependency version (#16189)

--- integration-test/src/main/java/org/apache/iotdb/it/env/cluster/env/AIEnv.java
+++ integration-test/src/main/java/org/apache/iotdb/it/env/cluster/env/AIEnv.java
@@ -31,7 +31,7 @@ public class AIEnv extends AbstractEnv {
 
   @Override
   public void initClusterEnvironment(int configNodesNum, int dataNodesNum) {
-    super.initEnvironment(configNodesNum, dataNodesNum, 1000, true);
+    super.initEnvironment(configNodesNum, dataNodesNum, 300, true);
     checkActivationStatus(configNodesNum + dataNodesNum + 1);
   }
 
