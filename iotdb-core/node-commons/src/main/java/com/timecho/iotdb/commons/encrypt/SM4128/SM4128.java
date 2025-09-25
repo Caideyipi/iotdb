35ad16fe1e174116945c8fc021bc9b492b7f6364
zhujt20 <zjt24@mails.tsinghua.edu.cn>
2025-09-25 11:08:14 +0800

    [TIMECHODB] Move encrypt algorithm codes to timecho-common

---
+++ iotdb-core/node-commons/src/main/java/com/timecho/iotdb/commons/encrypt/SM4128/SM4128.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.timecho.iotdb.commons.encrypt.SM4128;
+
+import org.apache.tsfile.encrypt.IDecryptor;
+import org.apache.tsfile.encrypt.IEncrypt;
+import org.apache.tsfile.encrypt.IEncryptor;
+
+public class SM4128 implements IEncrypt {
+
+  private final byte[] key;
+
+  public SM4128(byte[] key) {
+    this.key = key;
+  }
+
+  @Override
+  public IEncryptor getEncryptor() {
+    return new SM4128Encryptor(key);
+  }
+
+  @Override
+  public IDecryptor getDecryptor() {
+    return new SM4128Decryptor(key);
+  }
+}
