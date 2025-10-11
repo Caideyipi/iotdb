c0e1bfbde33a1846048614bfe7453f6273169b5b
Yongzao <yongzao@apache.org>
2025-10-11 08:35:36 +0800

    User userId to check whether the user is admin in ConfigNode (#16554)

--- integration-test/src/test/java/org/apache/iotdb/db/it/auth/IoTDBUserRenameIT.java
+++ integration-test/src/test/java/org/apache/iotdb/db/it/auth/IoTDBUserRenameIT.java
@@ -113,13 +113,26 @@ public class IoTDBUserRenameIT {
       adminStmt.execute("ALTER USER root RENAME TO user4");
       // We can create another root
       adminStmt.execute("CREATE USER root 'IoTDB@2025abc'");
+      // We can grant and revoke privilege to the new root
+      if (BaseEnv.TABLE_SQL_DIALECT.equals(dialect)) {
+        adminStmt.execute("GRANT SYSTEM TO USER root");
+        adminStmt.execute("REVOKE SYSTEM FROM USER root");
+      } else {
+        adminStmt.execute("GRANT SYSTEM ON root.** TO USER root");
+        adminStmt.execute("REVOKE SYSTEM ON root.** FROM USER root");
+      }
       // Ensure everything works
-      final String ans =
+      String ans =
           "0,admin,-1,-1,\n"
               + "10000,user4,-1,-1,\n"
               + "10001,user2,-1,-1,\n"
               + "10002,root,-1,-1,\n";
       ResultSet resultSet = adminStmt.executeQuery("LIST USER");
+      validateResultSet(resultSet, ans);
+      // Finally, the other root can be deleted
+      adminStmt.execute("DROP USER root");
+      ans = "0,admin,-1,-1,\n" + "10000,user4,-1,-1,\n" + "10001,user2,-1,-1,\n";
+      resultSet = adminStmt.executeQuery("LIST USER");
       validateResultSet(resultSet, ans);
     }
   }
