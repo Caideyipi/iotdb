37c7ab6528bc65e40f71f171e8c038ce2eb24c30
jintao zhu <105690440+zhujt20@users.noreply.github.com>
2025-10-13 17:55:27 +0800

    enable_grant_option first start take effect (#16572)

--- iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/conf/CommonDescriptor.java
+++ iotdb-core/node-commons/src/main/java/org/apache/iotdb/commons/conf/CommonDescriptor.java
@@ -527,6 +527,7 @@ public class CommonDescriptor {
     config.setSchemaEngineMode(globalConfig.schemaEngineMode);
     config.setTagAttributeTotalSize(globalConfig.tagAttributeTotalSize);
     config.setDiskSpaceWarningThreshold(globalConfig.getDiskSpaceWarningThreshold());
+    config.setEnableGrantOption(globalConfig.isEnableGrantOption());
   }
 
   public void loadAuditConfig(TAuditConfig auditConfig) {
