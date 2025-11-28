f439b141246f23046b226a35273a310a5241185c
Zhenyu Luo <zhenyu@apache.org>
2025-11-28 09:32:21 +0800

    perf: Optimize wide table write performance (#16699)
    * Refactor TableSchema handling to reduce conversion overhead
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix
    
    * update semanticCheck
    
    * fix: cache table schema by database and table
    
    * spotless
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix InsertStatementTest
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Revert "update"
    
    This reverts commit 7eb3b3a4062a8694afe1b491605d539349cd811d.
    
    * ignore object type
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * fix it
    
    * fix it
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * update
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/conf/IoTDBConfig.java
@@ -1134,8 +1134,6 @@ public class IoTDBConfig {
 
   private boolean loadActiveListeningEnable = true;
 
-  private long loadTableSchemaCacheSizeInBytes = 2 * 1024 * 1024L; // 2MB
-
   private long loadMeasurementIdCacheSizeInBytes = 2 * 1024 * 1024L; // 2MB
 
   private int loadTsFileSpiltPartitionMaxSize = 10;
@@ -4094,14 +4092,6 @@ public class IoTDBConfig {
 
   public void setLoadActiveListeningEnable(boolean loadActiveListeningEnable) {
     this.loadActiveListeningEnable = loadActiveListeningEnable;
-  }
-
-  public long getLoadTableSchemaCacheSizeInBytes() {
-    return loadTableSchemaCacheSizeInBytes;
-  }
-
-  public void setLoadTableSchemaCacheSizeInBytes(long loadTableSchemaCacheSizeInBytes) {
-    this.loadTableSchemaCacheSizeInBytes = loadTableSchemaCacheSizeInBytes;
   }
 
   public long getLoadMeasurementIdCacheSizeInBytes() {
